bf11acb43fbe270a75e4cb28c20a232efe3e926e
Prasad Bhawalkar <prasad.bhawalkar@gmail.com>
2026-03-23 22:03:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig(({ mode }) => {
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
+      'process.env.GAS_URL': JSON.stringify(env.GAS_URL),    
+            },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
